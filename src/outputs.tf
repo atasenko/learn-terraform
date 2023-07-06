177348bb59f1f024064ba7482e448b4d69332e17
Alexey Tasenko <fenixad@gmail.com>
2023-07-06 13:22:07 +0300

    fix Task 4

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,7 +1,6 @@
-output "external_ip_web" {
-  value = yandex_compute_instance.platform.network_interface.0.nat_ip_address
-}
-
-output "external_ip_db" {
-  value = yandex_compute_instance.platform-db.network_interface.0.nat_ip_address
+output "vm_ip" {
+  value = {
+    external_ip_web = yandex_compute_instance.platform.network_interface.0.nat_ip_address
+    external_ip_db  = yandex_compute_instance.platform-db.network_interface.0.nat_ip_address
+  }
 }
